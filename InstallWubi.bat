56263af0eb8616b318526f27cbcc338c7e443a7c
q24688@gmail.com <q24688@gmail.com@3c592d64-b25c-1f7a-fe69-eda617742dab>
2011-11-03 14:32:47 +0000

    从环境变量中取得正确用户名

--- InstallWubi.bat
+++ InstallWubi.bat
@@ -4,7 +4,7 @@ IF NOT EXIST "TableTextServiceSimplifiedZhengMa.txt" EXIT /B
 
 
 TAKEOWN /F "C:\Program Files\Windows NT\TableTextService\TableTextServiceSimplifiedZhengMa.txt" /A
-icacls "C:\Program Files\Windows NT\TableTextService\TableTextServiceSimplifiedZhengMa.txt" /grant ac:(D,WDAC)
+icacls "C:\Program Files\Windows NT\TableTextService\TableTextServiceSimplifiedZhengMa.txt" /grant %USERNAME%:(D,WDAC)
 DEL "C:\Program Files\Windows NT\TableTextService\TableTextServiceSimplifiedZhengMa.txt"
 COPY "TableTextServiceSimplifiedZhengMa.txt" "C:\Program Files\Windows NT\TableTextService\TableTextServiceSimplifiedZhengMa.txt"
 
@@ -12,7 +12,7 @@ COPY "TableTextServiceSimplifiedZhengMa.txt" "C:\Program Files\Windows NT\TableT
 IF NOT %PROCESSOR_ARCHITECTURE%==AMD64 EXIT /B
 
 TAKEOWN /F "C:\Program Files (x86)\Windows NT\TableTextService\TableTextServiceSimplifiedZhengMa.txt" /A
-icacls "C:\Program Files (x86)\Windows NT\TableTextService\TableTextServiceSimplifiedZhengMa.txt" /grant ac:(D,WDAC)
+icacls "C:\Program Files (x86)\Windows NT\TableTextService\TableTextServiceSimplifiedZhengMa.txt" /grant %USERNAME%:(D,WDAC)
 DEL "C:\Program Files (x86)\Windows NT\TableTextService\TableTextServiceSimplifiedZhengMa.txt"
 COPY "TableTextServiceSimplifiedZhengMa.txt" "C:\Program Files (x86)\Windows NT\TableTextService\TableTextServiceSimplifiedZhengMa.txt"
 
